0eed18e865f8bcd99754f2ca969934303d147b4b
loenisjunior <loenis45@gmail.com>
2023-08-22 20:29:15 -0300

    algoritomo do jogo foi introduzido mas tudo dentro da main

--- main.c
+++ main.c
@@ -1,9 +1,87 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-	int numDiscos;
+typedef struct {
+    int *discos;
+    int tamanho;
+} Torre;
+
+Torre *criarTorre(int capacidade) {
+    Torre *novaTorre = (Torre *)malloc(sizeof(Torre));
+    novaTorre->discos = (int *)malloc(capacidade * sizeof(int));
+    novaTorre->tamanho = 0;
+    return novaTorre;
+}
+
+void liberarTorre(Torre *torre) {
+    free(torre->discos);
+    free(torre);
+}
+
+void mostrarTorre(Torre *torre) {
+    for (int i = torre->tamanho - 1; i >= 0; i--) {
+        printf("%d: ", i + 1);
+        for (int j = 0; j < torre->discos[i]; j++) {
+            printf("X");
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void moverDisco(Torre *origem, Torre *destino) {
+    if (origem->tamanho > 0 && (destino->tamanho == 0 || origem->discos[origem->tamanho - 1] < destino->discos[destino->tamanho - 1])) {
+        destino->discos[destino->tamanho] = origem->discos[origem->tamanho - 1];
+        destino->tamanho++;
+        origem->tamanho--;
+    }
+}
+
+int main() {
+    int n;
     printf("Digite o número de discos: ");
-    scanf("%d", &numDiscos);
- 
-	return 0;
+    scanf("%d", &n);
+
+    Torre *torreA = criarTorre(n);
+    Torre *torreB = criarTorre(n);
+    Torre *torreC = criarTorre(n);
+
+    for (int i = n - 1; i >= 0; i--) {
+        torreA->discos[torreA->tamanho++] = i + 1;
+    }
+
+    int movimentos = 0;
+
+    while (1) {
+        printf("Estado atual:\n");
+        mostrarTorre(torreA);
+        mostrarTorre(torreB);
+        mostrarTorre(torreC);
+
+        int origem, destino;
+        printf("Digite a torre de origem e a torre de destino (ou 0 para sair): ");
+        scanf("%d %d", &origem, &destino);
+
+        if (origem == 0 || destino == 0) {
+            printf("Saindo...\n");
+            break;
+        }
+
+        moverDisco((origem == 1 ? torreA : (origem == 2 ? torreB : torreC)),
+                   (destino == 1 ? torreA : (destino == 2 ? torreB : torreC)));
+
+        movimentos++;
+
+        // Verificar se o jogador ganhou
+        if (torreC->tamanho == n) {
+            printf("Parabéns! Você ganhou em %d movimentos.\n", movimentos);
+            break;
+        }
+    }
+
+    liberarTorre(torreA);
+    liberarTorre(torreB);
+    liberarTorre(torreC);
+
+    return 0;
 }
--- torre.c
+++ torre.c
@@ -1,8 +1,55 @@
 #include "torre.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-struct prato* createPrato() {
-    struct prato* prato = (struct prato*)malloc(sizeof(struct prato));
-    prato->top = NULL;
-    return prato;
+// Função para mostrar o estado atual da torre
+void mostrarTorre(int torre[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d: ", i + 1);
+        for (int j = 0; j < torre[i]; j++) {
+            printf("X");
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+// Função para mover um disco de uma torre para outra
+void moverDisco(int origem, int destino, int torre[], int n) {
+    if (torre[origem] > 0 && (torre[destino] == 0 || torre[origem] < torre[destino])) {
+        torre[destino] = torre[origem];
+        torre[origem] = 0;
+    }
+}
+
+// Função para salvar o estado atual da torre em um arquivo
+void salvarJogo(int torre[], int n, const char *nomeArquivo) {
+    FILE *arquivo = fopen(nomeArquivo, "w");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo para salvar.\n");
+        return;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        fprintf(arquivo, "%d\n", torre[i]);
+    }
+    
+    fclose(arquivo);
+    printf("Jogo salvo com sucesso!\n");
+}
+
+// Função para carregar o estado da torre a partir de um arquivo
+void carregarJogo(int torre[], int n, const char *nomeArquivo) {
+    FILE *arquivo = fopen(nomeArquivo, "r");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo para carregar.\n");
+        return;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        fscanf(arquivo, "%d", &torre[i]);
+    }
+    
+    fclose(arquivo);
+    printf("Jogo carregado com sucesso!\n");
 }
